1369fb62885a6bbb92fbce08be4840292f0ee10d
rgraba <127319214+rgraba@users.noreply.github.com>
2023-04-19 14:08:24 +0000

    4_3

--- Main.java
+++ Main.java
@@ -26,9 +26,11 @@ class Main {
               case 1:
                 System.out.println("Podaj Imię:");
                 String imie = scanner.next();
+                System.out.println("Podaj Nazwisko:");
+                String nazwisko = scanner.next();
                 System.out.println("Podaj wiek:");
                 int wiek = scanner.nextInt();
-                s.addStudent(new Student(imie, wiek));
+                s.addStudent(new Student(imie, nazwisko, wiek));
                 break;
               case 0:
                 System.exit(0);
@@ -37,7 +39,6 @@ class Main {
                 for(Student current : students) {
                 System.out.println(current.ToString());
                 }
-                System.exit(0);
               default:
                 System.out.println("Zły wybór");
                 break;
